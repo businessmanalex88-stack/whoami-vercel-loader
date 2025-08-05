9bda7f07b1d07920093437d1f56132e8f63c89ea
businessmanalex88-stack <businessmanalex88@gmail.com>
2025-08-06 04:58:08 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,6 +1,15 @@
 export default async function handler(req, res) {
+  const userAgent = req.headers['user-agent']?.toLowerCase() || "";
+
+  // Blokir akses jika bukan dari Roblox Executor
+  if (!userAgent.includes("roblox")) {
+    return res.status(403).send("Access Denied");
+  }
+
+  // Ambil script dari GitHub
   const response = await fetch("https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/refs/heads/main/Launcher%20Bypass%20All");
   const code = await response.text();
+
   res.setHeader("Content-Type", "text/plain");
   res.send(code);
 }
