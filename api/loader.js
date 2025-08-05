31901c0b0f2968d9379aa93d2a88048c953266f8
businessmanalex88-stack <businessmanalex88@gmail.com>
2025-08-06 05:18:49 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,15 +1,15 @@
 export default async function handler(req, res) {
-  const userAgent = req.headers['user-agent']?.toLowerCase() || "";
+  const _0x3f3c = ['roblox', 'user-agent', 'toLowerCase', 'headers', 'includes', 'Access Denied', 'status', 'send', 'https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/refs/heads/main/Launcher%20Bypass%20All', 'text/plain', 'Content-Type'];
+  const _0x1a2f = function (i) { return _0x3f3c[i]; };
 
-  // Blokir akses jika bukan dari Roblox Executor
-  if (!userAgent.includes("roblox")) {
-    return res.status(403).send("Access Denied");
+  const ua = req[_0x1a2f(3)][_0x1a2f(1)]?.[_0x1a2f(2)]() || "";
+  if (!ua[_0x1a2f(4)](_0x1a2f(0))) {
+    return res [_0x1a2f(7)](_0x1a2f(5));
   }
 
-  // Ambil script dari GitHub
-  const response = await fetch("https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/refs/heads/main/Launcher%20Bypass%20All");
-  const code = await response.text();
+  const gh = await fetch(_0x1a2f(8));
+  const sc = await gh.text();
 
-  res.setHeader("Content-Type", "text/plain");
-  res.send(code);
+  res.setHeader(_0x1a2f(10), _0x1a2f(9));
+  res.send(sc);
 }
