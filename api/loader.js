1a0d8d61bfee44cd3d387625aa783a865416b4b7
businessmanalex88-stack <businessmanalex88@gmail.com>
2025-08-06 04:36:15 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,4 +1,9 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(`loadstring(game:HttpGet("https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/main/Radiant.lua"))()`);
+export default async function handler(req, res) {
+  const scriptUrl = "https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/main/Radiant.lua";
+
+  const response = await fetch(scriptUrl);
+  const script = await response.text();
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(`loadstring([[${script}]])()`);
 }
