7c0738fa8079b48aa5f8ef88812aa65fe6bb3190
businessmanalex88-stack <businessmanalex88@gmail.com>
2025-08-06 04:41:58 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
   const scriptUrl = "https://raw.githubusercontent.com/businessmanalex88-stack/BypassWhoAmI/main/Radiant.lua";
-
   try {
     const response = await fetch(scriptUrl);
     const script = await response.text();
-
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
     res.setHeader("Cache-Control", "no-store");
     res.status(200).send(`loadstring([[${script}]])()`);
