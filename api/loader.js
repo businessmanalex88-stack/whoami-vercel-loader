351763dc2e04cb2c80533648f5ed589512af58f7
businessmanalex88-stack <businessmanalex88@gmail.com>
2025-09-21 14:30:47 +0700

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -3,13 +3,22 @@ export default async function handler(req, res) {
   const _0x1a2f = function (i) { return _0x3f3c[i]; };
 
   const ua = req[_0x1a2f(3)][_0x1a2f(1)]?.[_0x1a2f(2)]() || "";
-  if (!ua[_0x1a2f(4)](_0x1a2f(0))) {
-    return res [_0x1a2f(7)](_0x1a2f(5));
+  
+  // Check if request is from Roblox
+  if (ua[_0x1a2f(4)](_0x1a2f(0))) {
+    const gh = await fetch(_0x1a2f(8));
+    const sc = await gh.text();
+
+    res.setHeader(_0x1a2f(10), _0x1a2f(9));
+    res.send(sc);
+  } 
+  // If not Roblox, check if it's a browser and redirect
+  else if (ua.includes('mozilla') || ua.includes('chrome') || ua.includes('safari')) {
+    res.writeHead(302, { 'Location': 'https://whoamibypass.online' });
+    res.end();
   }
-
-  const gh = await fetch(_0x1a2f(8));
-  const sc = await gh.text();
-
-  res.setHeader(_0x1a2f(10), _0x1a2f(9));
-  res.send(sc);
+  // Deny access for other user-agents
+  else {
+    return res[_0x1a2f(6)](403)[_0x1a2f(7)](_0x1a2f(5));
+  }
 }
